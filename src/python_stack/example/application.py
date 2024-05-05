1b525b0d2cb4a7d9d0f52f513694ac6fbb27319c
miragecentury <victorien.vanroye@gmail.com>
2024-05-05 18:14:24 +0200

    refactor: override _configure_inject in example

--- src/python_stack/example/application.py
+++ src/python_stack/example/application.py
@@ -2,6 +2,7 @@
 Example Application class for the Python Stack.
 """
 
+from inject import Binder
 from python_stack.core.application import AbstractApplication
 
 
@@ -13,3 +14,11 @@ class Application(AbstractApplication):
     _version = "0.1.0"
     _title = "Python Stack Example"
     _description = "Example Python Stack Application"
+
+    def _configure_inject(self, binder: Binder) -> None:
+        """
+        Overrides the parent method to configure the dependency injection container for
+        the Application "Example".
+        """
+        super()._configure_inject(binder)
+        binder.bind(Application, self)
